848f21273505f89cb0751e41bde2a64900b293be
Anastasiya.Ivanova <agb66666@gmail.com>
2022-04-14 10:28:05 +0300

    Added the solutions.

---
+++ Inheritance-Lab/02.MultipleInheritance/StartUp.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Farm
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            
+            Puppy pu=new Puppy();
+            pu.Bark();
+            pu.Eat();
+            pu.Weep();
+        }
+    }
+}
